c6de9228b328c8e41efcc6790ac73f7653b5b3ae
7Aylon <athlarster@gmail.com>
2021-01-23 19:08:17 +0100

    little change after big

--- src/Components/Home-Navigation/Buttons/ReserveLessonButton.jsx
+++ src/Components/Home-Navigation/Buttons/ReserveLessonButton.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../common/Button';
+import { UserContext } from '../../../contexts/UserContext'
+
 
 function ReserveLessonButton(props) {
+    const { user } = useContext(UserContext)
+
     return (
-        <Link to="calendar"><Button text="Zapisz mnie" btnClass="reserve-button" /></Link>
+        <Link to="calendar"><Button text={!user.isAdmin ? "Zapisz mnie" : "Kalendarz"} btnClass="reserve-button" /></Link>
     );
 }
 
