24142e843c3e3f9c42f0c98e9d47caf38facc823
7Aylon <athlarster@gmail.com>
2021-02-08 22:19:09 +0100

    changes in terms and conditions

--- src/Components/TermsAndConditions.jsx
+++ src/Components/TermsAndConditions.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import useScrollUp from '../hooks/useScroller';
 import regulamin from '../static/regulamin.jpg'
 import MainContainer from './common/MainContainer';
 import './TermsAndPrivacy.css'
 
 function TermsAndConditions(props) {
+    useScrollUp()
+
     return (
         <MainContainer navBar title="Regulamin">
             <div className="terms-and-privacy">
-                <div className="terms-and-privacy-picture">
-                    <img src={regulamin} alt="" />
-                </div>
+                <img className="terms-and-privacy-picture" src={regulamin} alt="" />
             </div>
         </MainContainer>
     );
